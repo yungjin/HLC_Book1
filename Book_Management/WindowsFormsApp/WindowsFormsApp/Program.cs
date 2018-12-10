b9a9b87499581b8557002aacd2badb119ebe8dc3
영진 최 <chyj0318@naver.com>
2018-12-10 16:42:58 +0900

    기본

--- Book_Management/WindowsFormsApp/WindowsFormsApp/Program.cs
+++ Book_Management/WindowsFormsApp/WindowsFormsApp/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main_Form());
+            Application.Run(new Sample_Form2());
         }
     }
 }
--- Book_Management/WindowsFormsApp/WindowsFormsApp/Sample_Form2.Designer.cs
+++ Book_Management/WindowsFormsApp/WindowsFormsApp/Sample_Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp
 {
-    partial class user1
+    partial class Sample_Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -31,7 +31,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "user1";
+            this.Text = "Sample_Form2";
         }
 
         #endregion
